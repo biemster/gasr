b70e3156d3e3dbe5ac0120a67c45718bd49d60e3
Lars Beemster <biemster@users.noreply.github.com>
2025-01-14 15:40:34 +0100

    implement patching dynamic linker

--- README.md
+++ README.md
@@ -9,11 +9,24 @@ not actively maintained. Since ChromeOS is linux under the hood, Windows is not 
 use the `chrome-browser` branch.
 
 ##### Prepare:
-Use the `prep.py` script to download the library for your platform and the model language of your choosing. If your platform is not
-available, please respond in https://github.com/biemster/gasr/issues/24
+Use the `prep.py` script to download the library for your platform, the language model of your choosing and patch the dynamic linker
+to accept the relative relocations used in `libsoda.so`. Some examples are:
+```bash
+./prep.py -c # check if the dynamic linker is copied, patched and ready
+```
+```bash
+./prep.py -c -p hana # check if libsoda.so for RPi4 is downloaded, fixed and ready
+```
+```bash
+./prep.py -c -p hana -l "en-us" # check if libsoda.so for RPi4 is ready, and the en-us model
+```
+```bash
+./prep.py -s -p hana # setup the ld-linux interpreter and libsoda.so for RPi4
+```
+If your platform is not available, please respond in https://github.com/biemster/gasr/issues/24
 
 ##### Run:
 ```bash
-arecord -Dplughw:3,0 -fS16_LE -c1 -r16000 | ./gasr.py 2>/dev/null
+arecord -Dplughw:3,0 -fS16_LE -c1 -r16000 | ./ld-linux.so $(which python3) gasr.py 2>/dev/null
 ```
 where `hw:3,0` should be changed to where your microphone lives in your ALSA setup.
--- prep.py
+++ prep.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import os
+import glob
 import argparse
+import subprocess
+import hashlib
 
 PLATFORMS = {
     'hana': 'RPi4 (aarch64 Cortex-A72)',
@@ -10,6 +14,16 @@ PLATFORMS = {
     'zork': 'AMD',
 }
 
+LIB_HASHES = { # ['orig', 'fixed']
+    'hana': ['', ''],
+    'octopus': ['', ''],
+    'rammus': ['', ''],
+    'hatch': ['', ''],
+    'volteer': ['', ''],
+    'zork': ['', ''],
+}
+
+LEGACY_VERSION = 'df24d2'
 LEGACY_LANGUAGES = [
     'en-us',
     'ja-jp',
@@ -44,6 +58,7 @@ LEGACY_LANGUAGES = [
     'vi-vn',
 ]
 
+CONCH_VERSION = 'cnch24d3'
 CONCH_LANGUAGES = [
     'da-dk',
     'de-at',
@@ -77,17 +92,23 @@ LATEST = '133-16151.2.0'
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--show', help='Show platform and language choices', action='store_true')
-    parser.add_argument('-p', '--platform', help='Show download links for specified platform', choices=PLATFORMS.keys())
-    parser.add_argument('-l', '--language', help='Show download links for specified language', choices=set(LEGACY_LANGUAGES+CONCH_LANGUAGES))
+    parser.add_argument('-a', '--available', help='Show all available platforms and languages', action='store_true')
+    parser.add_argument('-c', '--check', help='Check if everything is ready to run', action='store_true')
+    parser.add_argument('-s', '--setup', help='Download and setup everything', action='store_true')
+    parser.add_argument('-p', '--platform', help='Platform to download/check the library for', choices=PLATFORMS.keys())
+    parser.add_argument('-l', '--language', help='ASR model language', choices=set(LEGACY_LANGUAGES+CONCH_LANGUAGES))
     args = parser.parse_args()
 
-    if args.show:
+    if args.available:
         print('Available platforms:')
         for p,d in PLATFORMS.items():
             print(f'* {p}: ({d})')
         print('Available languages:')
         print(f'* {set(LEGACY_LANGUAGES+CONCH_LANGUAGES)}')
+    elif args.check:
+        check(args.platform, LATEST, args.language)
+    elif args.setup:
+        setup(args.platform, LATEST, args.language)
     elif args.platform and args.language:
         print(library_url(args.platform, LATEST))
         print(model_url(args.platform, LATEST, args.language))
@@ -100,13 +121,194 @@ def library_url(platform, release):
     else:
         return None
 
-def model_url(platform, release, language):
-    if language in CONCH_LANGUAGES:
-        return f'{URL}{platform}-release/R{release}/dlc-scaled/libsoda-model-{language}-cnch24d3/package/dlc.img'
-    elif language in LEGACY_LANGUAGES:
-        return f'{URL}{platform}-release/R{release}/dlc-scaled/libsoda-model-{language}-df24d2/package/dlc.img'
+def library_name_base(platform, release):
+    if platform in PLATFORMS.keys():
+        return f'libsoda_{platform}_R{release[:3]}'
     else:
         return None
+
+def model_url(platform, release, language):
+    res = None
+    if language in CONCH_LANGUAGES:
+        res = f'{URL}{platform}-release/R{release}/dlc-scaled/libsoda-model-{language}-{CONCH_VERSION}/package/dlc.img'
+    elif language in LEGACY_LANGUAGES:
+        res = f'{URL}{platform}-release/R{release}/dlc-scaled/libsoda-model-{language}-{LEGACY_VERSION}/package/dlc.img'
+    return res
+
+def model_name_full(language):
+    res = None
+    if language in CONCH_LANGUAGES:
+        res = f'SODAModels_{language}_{CONCH_VERSION}'
+    elif language in LEGACY_LANGUAGES:
+        res = f'SODAModels_{language}_{LEGACY_VERSION}'
+    return res
+
+def download(url, outname):
+    import requests
+    with open(outname, 'wb') as f:
+        response = requests.get(url, stream=True)
+        total_length = int(response.headers.get('content-length'))
+        bar_length = 50 # char length of progress bar
+    
+        if total_length is None: # no content length header
+            f.write(response.content)
+        else:
+            received = 0
+            for data in response.iter_content(chunk_size=4096):
+                received += len(data)
+                f.write(data)
+                progress = int((received / total_length) *bar_lenght)
+                print(f'{outname} ({total_length // 1024 // 1024}M): [{'='*progress}{' '*(bar_lenght-progress)}]', end='\r')
+        print()
+
+def extract_library(lib_base):
+    subprocess.run(['7z', 'e', f'{lib_base}.img', '-so', 'root/libsoda.so', '>', f'{lib_base}.so'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(['ln', '-s', f'{lib_base}.so', 'libsoda.so'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def extract_model(model_name):
+    subprocess.run(['7z', 'x', f'{model_name}.img', 'root'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(['mv', 'root', model_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def bitflip(platform, lib_base):
+    if not LIB_HASHES[platform][1]:
+        print(f'Fixing library on platform {platform} is not implemented yet, please have a look at https://github.com/biemster/gasr/issues/24')
+
+def has_library(platform, lib_base):
+    has_img = False
+    has_lib = False
+    is_fixed = False
+    has_symlink = False
+    if os.path.exists(f'{lib_base}.img'):
+        has_img = True
+    if os.path.exists(f'{lib_base}.so'):
+        h = get_hash(f'{lib_base}.so')
+        if h == LIB_HASHES[0]:
+            has_lib = True
+        if h == LIB_HASHES[1]:
+            has_lib = True
+            is_fixed = True
+        if os.path.islink('libsoda.so') and get_hash('libsoda.so') == h:
+            has_symlink = True
+
+    return has_img,has_lib,is_fixed,has_symlink
+
+def has_model(model_name):
+    has_img = False
+    has_dir = False
+    has_symlink = False
+    if os.path.exists(f'{model_name}.img'):
+        has_img = True
+    if os.path.isdir(model_name):
+        has_img = True
+    if os.path.islink('SODAModels') and os.path.exists('SODAModels/metadata') and os.path.exists(f'{model_name}/metadata') and get_hash('SODAModels/metadata') == get_hash(f'{model_name}/metadata'):
+        has_symlink = True
+    return has_img,has_dir,has_symlink
+
+def has_linker():
+    has_linker = False
+    is_fixed = False
+    linker = glob.glob('ld-linux*')
+    if len(linker):
+        has_linker = True
+        linker_strings = subprocess.check_output(['strings', linker[0]]).decode().split('\n')
+        if 'xibc.so.' in linker_strings:
+            is_fixed = True
+    return has_linker, is_fixed
+
+def check(platform, release, language):
+    if platform:
+        lib_base = library_name_base(platform, release)
+        lib_ready = has_library(platform, lib_base)
+        print(f'Library has .img: ..................... {lib_ready[0]}')
+        print(f'Library has .so: ...................... {lib_ready[1]}')
+        print(f'Library .so is fixed: ................. {lib_ready[2]}')
+        print(f'Library .so is symlinked: ............. {lib_ready[3]}')
+    if language:
+        model_name = model_name_full(language)
+        model_ready = has_model(model_name)
+        print(f'Language has .img: .................... {model_ready[0]}')
+        print(f'Language .img is extracted to dir:..... {model_ready[1]}')
+        print(f'Language dir is symlinked: ............ {model_ready[2]}')
+    linker_ready = has_linker()
+    print(f'Dynamic linker ld-linux is copied here: {linker_ready[0]}')
+    print(f'Dynamic linker ld-linux is fixed: ..... {linker_ready[1]}')
+
+def get_hash(fname):
+    sha1 = hashlib.sha1()
+    with open(fname, 'rb') as f:
+        while True:
+            data = f.read(4096)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1.hexdigest()
+
+def setup_library(platform, release):
+    lib_base = library_name_base(platform, release)
+    lib_ready = has_library(platform, lib_base)
+    if not lib_ready[0]:
+        download(library_url(platform, release), f'{lib_base}.img')
+    if not lib_ready[1]:
+        extract_library(lib_base)
+    if not lib_ready[2]:
+        bitflip(platform, lib_base)
+    if not lib_ready[3]:
+        subprocess.run(['ln', '-s', '', 'libsoda.so'])
+
+def setup_model(platform, release, language):
+    model_name = model_name_full(language)
+    model_ready = has_model(model_name)
+    if not model_ready[0]:
+        download(model_url(platform, release, language), f'{model_name}.img')
+    if not model_ready[1]:
+        extract_model(model_name)
+    if not model_ready[2]:
+        subprocess.run(['ln', '-s', model_name, 'SODAModels'])
+
+def setup_linker():
+    linker_orig = None
+    ldd_env = subprocess.check_output(['ldd', '/usr/bin/env']).decode().replace('\t','').split('\n')
+    for l in ldd_env:
+        if 'ld-linux' in l:
+            l_split = l.split(' => ')
+            if 'ld-linux' in l_split[0]:
+                linker_orig = l_split[0]
+                break
+    if linker_orig:
+        print(f'Found linker {linker_orig}, copying it here and disabling "DT_RELR without GLIBC_ABI_DT_RELR" warning')
+        with open('ld-linux.so', 'wb') as outf:
+            with open(linker_orig, 'rb') as inf:
+                while True:
+                    data = inf.read(4096)
+                    if not data:
+                        break
+                    dhex = data.hex()
+                    if 'libc.so.\0'.encode().hex() in dhex:
+                        print(f'Found "libc.so.", replacing with "xibc.so."')
+                        data = bytes.fromhex(dhex.replace('libc.so.\0'.encode().hex(), 'xibc.so.\0'.encode().hex()))
+                    outf.write(data)
+    else:
+        print(f'Linker ld-linux not found! This is an error, please report it')
+
+def setup(platform, release, language):
+    if platform:
+        lib_base = library_name_base(platform, release)
+        lib_ready = has_library(platform, lib_base)
+        if not all(lib_ready):
+            setup_library(platform, release)
+        else:
+            print(f'Platform {platform} already fully set up.')
+    if language:
+        model_name = model_name(platform, release, language)
+        model_ready = has_model(model_name)
+        if not all(model_ready):
+            setup_model(platform if platform else PLATFORMS.keys()[0], release, language)
+        else:
+            print(f'Language {language} already fully set up.')
+    linker_ready = has_linker()
+    if not linker_ready[0] or not linker_ready[1]:
+        print('Dynamic linker not setup yet, doing that now')
+        setup_linker()
 
 if __name__ == '__main__':
     main()
